d248a2f0bab4d867d829aa6756610718a8cb6ad0
Pip <pip.harsveld@hva.nl>
2023-05-12 14:32:25 +0200

    Update captions2.js

--- public/scripts/captions2.js
+++ public/scripts/captions2.js
@@ -94,6 +94,71 @@ export let captions = [
         time: 54.5,
         character: "Koning Willem Alexander",
     }
+
+
+    ,
+    {
+        caption: 'Ja',
+        time: 57,
+        character: "Edwin Evers",
+    },
+    {
+        caption: 'U zat in stadion, ik ook, en ik dacht, ik moet een uur daarna... ja, toen nog de kroonprins, interviewen.',
+        time: 58,
+        character: "Edwin Evers",
+    },
+    {
+        caption: 'Hoe gaan we dit doen? Want u bent een sport fanaat,',
+        time: 64,
+        character: "Edwin Evers",
+    },
+    {
+        caption: 'ik denk nou, dit wordt een drama, die heeft er natuurlijk hÉlemaal geen zin in.',
+        time: 66,
+        character: "Edwin Evers",
+    },
+    {
+        caption: 'Ik had nog niet de nacht slecht geslapen toen ik met u sprak,',
+        time: 69,
+        character: "Koning Willem Alexander",
+    },
+    {
+        caption: 'dus de volgende dag was het interview erger geweest, denk ik, want nacht niet slapen na zo"n wissel uh...',
+        time: 73,
+        character: "Koning Willem Alexander",
+    },
+    { caption: "dat was het voor het interview nog slechter geweest.", time: 77 },
+    { caption: 'Edwin Evers: En hoe is dat vandaag?', time: 79.5 },
+    { caption: 'Koning Willem Alexander: Vandaag gaat heel goed', time: 81 },
+    { caption: 'Edwin Evers: Topfit?', time: 82 },
+    { caption: 'Koning Willem Alexander: Topfit, helemaal klaar om te beginnen aan dit uh.. interessante avontuur.', time: 82.5 },
+    { caption: 'Edwin Evers: Ja', time: 86 },
+    { caption: '[Tune]', time: 87 },
+    { caption: 'Edwin Evers: Een podcast. Ik vond dat echt meteen leuk, want het is...', time: 89.5 },
+    { caption: 'já, voor mij voelt dat natuurlijk een beetje als radio.', time: 91.5 },
+    { caption: 'U heeft ook de nodige ervaring met uh... radio', time: 93.5 },
+    { caption: 'en het is natuurlijk een mooi nieuw uh...', time: 96 },
+    { caption: 'medium, dat erg populair is op dit moment. Heeft u zelf de podcast al ontdekt, eigenlijk?', time: 98.5 },
+    { caption: 'Koning Willem Alexander: Nee, eerlijk gezegd heb ik podcast nog niet ontdekt', time: 102.5 },
+    { caption: 'Edwin Evers: Nee', time: 105 },
+    { caption: "Koning Willem Alexander: en het verbaast me ook ontzettend dat het zo'n succesvolle formule is.", time: 105.5 },
+    { caption: 'In de tijd dat alles snel, snel, snel moet gaan. Twitter 280 tekens uh... vroeger nog 140,', time: 108.5 },
+    { caption: 'kon je alles in doen... en als je meer dan dat had te zeggen, dan werd er niet meer geluisterd ', time: 114 },
+    { caption: 'en toch is de podcast is uh... neemt tijd, langzaam, mensen luisteren ernaar, het is een heel populair medium.', time: 119 },
+    { caption: 'Ja, en uh... ik denk dat het heel goed medium is ook om te reflecteren op tien jaar koningschap.', time: 125 },
+    { caption: 'Edwin Evers: Ja, en dat gaan we per jaar doen. We beginnen zo meteen, in 2013. ', time: 129.5 },
+    { caption: 'Ik heb natuurlijk de nodige instructies meegekregen. ', time: 134 },
+    { caption: 'We gaan het over het koningschap hebben, uw koningschap,', time: 136 },
+    { caption: 'tien jaar lang de Koning, maar niet-niet teveel privé...', time: 139 },
+    { caption: 'Koning Willem Alexander: Maar... U bent toch niet iemand die echt de instructies... helemaal...', time: 142.5 },
+    { caption: 'Edwin Evers: Nee klopt, maar ik wil even de piketpalen slaan [Hahaha] ', time: 144 },
+    { caption: 'Koning Willem Alexander: [Hahaha]', time: 145.5 },
+    { caption: 'Koning Willem Alexander: Ja nee maar U bent toch niet iemand die alle instructies meteen overneemt toch?', time: 146.5 },
+    { caption: 'Edwin Evers: Zal ik de iPad gewoon wegleggen nu?!', time: 149.5 },
+    { caption: 'Koning Willem Alexander: [Hahaha]', time: 150.5 },
+    { caption: 'Edwin Evers: Nee, want uh... niet te veel privé werd er gezegd.', time: 152 },
+    { caption: 'Koning Willem Alexander: Nou, dat uh..., dat mag wel, maar uh...', time: 154 },
+    { caption: 'Edwin Evers: Maar U antwoord niet', time: 157 }
 ];
 
 
